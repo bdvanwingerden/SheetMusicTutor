b774d5e7fed02a643ac858397860e044c3303c0d
bdvanwingerden <bdvanwingerden1@catamount.wcu.edu>
2017-10-23 19:56:48 -0400

    Clean-Up

--- Sheet_Music_Tutor/app/src/main/java/vanwingerdenbarrier/sheetmusictutor/Key/KeySound2.java
+++ Sheet_Music_Tutor/app/src/main/java/vanwingerdenbarrier/sheetmusictutor/Key/KeySound2.java
@@ -1,10 +1,12 @@
-package vanwingerdenbarrier.sheetmusictutor;
+package vanwingerdenbarrier.sheetmusictutor.Key;
 import android.content.Context;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Build;
 import android.view.View;
 import android.widget.Button;
+
+import vanwingerdenbarrier.sheetmusictutor.R;
 
 /**
  * Created by Doriangh4 on 9/28/2017.
--- Sheet_Music_Tutor/app/src/main/java/vanwingerdenbarrier/sheetmusictutor/KeyFragment.java
+++ Sheet_Music_Tutor/app/src/main/java/vanwingerdenbarrier/sheetmusictutor/KeyFragment.java
@@ -4,13 +4,13 @@ package vanwingerdenbarrier.sheetmusictutor;
  * Class that implements the keyboard view
  */
 import android.app.Fragment;
-import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import vanwingerdenbarrier.sheetmusictutor.Key.KeySound2;
 
 
 public class KeyFragment extends Fragment {
