801d42b8f74984c5a2cc16cf1703d893aa695cbf
bdvanwingerden <bdvanwingerden1@catamount.wcu.edu>
2017-10-23 19:07:24 -0400

    Together at last
    Both the key Fragment & staff fragment are finally together!

--- Sheet_Music_Tutor/app/src/main/java/vanwingerdenbarrier/sheetmusictutor/KeyFragment.java
+++ Sheet_Music_Tutor/app/src/main/java/vanwingerdenbarrier/sheetmusictutor/KeyFragment.java
@@ -1,12 +1,64 @@
 package vanwingerdenbarrier.sheetmusictutor;
+/**
+ * Created by Dorian Barrier 9/28/17
+ * Class that implements the keyboard view
+ */
 import android.app.Fragment;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
-public class KeyFragment extends Fragment{
-    public View onCreateView(/*TODO add parameters*/){
 
+public class KeyFragment extends Fragment {
 
-        //TODO make a view to return
-        return null;
+    /*Buttons that correspond with each sound*/
+    Button a,b,c,d,e,f,g,cs,ds,fs,gs,as;
+
+    /**Instance of KeySoundAPI*/
+    KeySound2 keysound;
+
+    /**
+     * Finds the reference to the buttons
+     * calls keysound2 to implement sound
+     * @param savedInstanceState
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.keyboard_fragment, container, false);
+    }//end onCreate
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+
+        View v = getView();
+
+        //Factory method that does this
+        a = (Button) v.findViewById(R.id.a);
+        b = (Button) v.findViewById(R.id.b);
+        c = (Button) v.findViewById(R.id.c);
+        d = (Button) v.findViewById(R.id.d);
+        e = (Button) v.findViewById(R.id.e);
+        f = (Button) v.findViewById(R.id.f);
+        g = (Button) v.findViewById(R.id.g);
+        cs = (Button) v.findViewById(R.id.cs);
+        ds = (Button) v.findViewById(R.id.ds);
+        as = (Button) v.findViewById(R.id.as);
+        fs = (Button) v.findViewById(R.id.fs);
+        gs = (Button) v.findViewById(R.id.gs);
+
+        //create instance of KeySound2 API and pass buttons and context
+        keysound = new KeySound2(a,b,c,d,e,f,g,cs,ds,fs,gs,as, getActivity());
+        keysound.createPool();
+
     }
-}
+
+}//end class keyboard
+
+
