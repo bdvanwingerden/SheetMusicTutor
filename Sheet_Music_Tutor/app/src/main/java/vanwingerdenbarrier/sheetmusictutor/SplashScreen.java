83ac5ca981d35ff13466cfda585fb4fc49413ea1
doriangh3 <doriangh3@gmail.com>
2018-02-10 18:11:19 -0500

    SplashScreen Done
    n/a

--- Sheet_Music_Tutor/app/src/main/java/vanwingerdenbarrier/sheetmusictutor/SplashScreen.java
+++ Sheet_Music_Tutor/app/src/main/java/vanwingerdenbarrier/sheetmusictutor/SplashScreen.java
@@ -1,13 +1,26 @@
 package vanwingerdenbarrier.sheetmusictutor;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashScreen extends AppCompatActivity {
 
+    private static int SPLASH_TIME_OUT = 5000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                Intent menuIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(menuIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
