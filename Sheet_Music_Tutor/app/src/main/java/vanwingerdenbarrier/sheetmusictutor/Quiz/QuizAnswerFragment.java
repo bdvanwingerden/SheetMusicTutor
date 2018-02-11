ba03e8719682d1c8d960276a18903416873ff876
bvanwingerden <bdvanwingerden1@catamount.wcu.edu>
2018-02-11 16:32:04 -0500

    Combo mode lookin good and working good - minor changes

--- Sheet_Music_Tutor/app/src/main/java/vanwingerdenbarrier/sheetmusictutor/Quiz/QuizAnswerFragment.java
+++ Sheet_Music_Tutor/app/src/main/java/vanwingerdenbarrier/sheetmusictutor/Quiz/QuizAnswerFragment.java
@@ -6,6 +6,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
+import android.view.ContextThemeWrapper;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
